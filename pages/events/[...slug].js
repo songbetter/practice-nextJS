b6822e90f2a0c51bcbc1e6ff05c083bf90a8aeeb
songbetter <5ongnaeu17@gmail.com>
2022-09-25 12:18:32 +0900

    feat: EventSearchedPage 생성

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,12 +1,45 @@
 import { useRouter } from 'next/router'
 
-export default function EventPage() {
+import { EVENT_LIST } from '.'
+import EventList from '../../components/event/EventList'
+import EventSearch from '../../components/event/EventSearch'
+
+export default function EventSearchedPage() {
   const router = useRouter()
   const [year, month] = router.query.slug
 
+  const EventFilterdItems = EVENT_LIST.filter(
+    (event) =>
+      event.date.split('-')[0] === year && event.date.split('-')[1] === month,
+  )
+
+  if (!year || !month) {
+    return <p>Loading....</p>
+  }
+
+  if (
+    isNaN(year) ||
+    isNaN(month) ||
+    +year < 2021 ||
+    +month > 12 ||
+    +month < 1
+  ) {
+    return <p>Invalid filter, Please adjust your values!</p>
+  }
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`
+    router.push(fullPath)
+  }
   return (
     <div>
-      EventPage {year}-{month}
+      <h1>EventSearchedPage</h1>
+      <h2>
+        Event in {year}-{month}
+      </h2>
+      <EventSearch onSearch={findEventsHandler} />
+      <EventList items={EventFilterdItems} />
+      {!EventFilterdItems.length && <p>No data</p>}
     </div>
   )
 }
