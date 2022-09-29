dd8fd4751d3534e7e52b144b6d7456c303010efe
songbetter <5ongnaeu17@gmail.com>
2022-09-29 23:51:10 +0900

    feat: Modal 컴포넌트 생성

---
+++ components/ui/modal.js
@@ -0,0 +1,80 @@
+import { useContext } from 'react'
+
+import { ModalContext } from '../../store/modal-context'
+import Button from './button'
+
+export default function Modal({ children }) {
+  const { closeModal } = useContext(ModalContext)
+
+  return (
+    <ModalBackground>
+      <ModalBox>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <CloseIcon clickEvent={closeModal} />
+
+          {children}
+        </div>
+      </ModalBox>
+    </ModalBackground>
+  )
+}
+
+function ModalBackground({ children }) {
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        width: '100%',
+        height: '100%',
+        background: 'rgba(0,0,0,0.4)',
+      }}
+    >
+      {children}
+    </div>
+  )
+}
+
+function ModalBox({ children }) {
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        margin: 'auto',
+        width: '50%',
+        height: '50%',
+        background: '#fff',
+        color: 'black',
+      }}
+    >
+      {children}
+    </div>
+  )
+}
+
+function CloseIcon({ clickEvent }) {
+  return (
+    <div
+      style={{
+        alignSelf: 'flex-end',
+        fontSize: '3rem',
+        margin: '0.5rem 1rem',
+        cursor: 'pointer',
+      }}
+      onClick={clickEvent}
+    >
+      🆇
+    </div>
+  )
+}
